7307ddc6b0763d6d705d16d9c662e5090f6ecc14
Steve <138328427+smajewski1980@users.noreply.github.com>
2024-10-29 21:06:24 -0400

    got card links done and randomized fl4 animation timing adj spotlight

--- projects/projects_script.js
+++ projects/projects_script.js
@@ -36,6 +36,14 @@ const descriptions = [
     desc: "description 7 - when this is a completed page, there will be this much text or less in this section",
   },
 ];
+const ltFl4Wrapper = document.querySelector(".fl4-w-1 .lt-fl4-wr");
+const ltFlame4 = document.querySelector(".fl4-w-1 .lt-fl4-wr .flame4");
+const rtFl4Wrapper = document.querySelector(".fl4-w-1 .rt-fl4-wr");
+const rtFlame4 = document.querySelector(".fl4-w-1 .rt-fl4-wr .flame4");
+const ltFl4WrapperRear = document.querySelector(".fl4-w-2 .lt-fl4-wr");
+const ltFlame4Rear = document.querySelector(".fl4-w-2 .lt-fl4-wr .flame4");
+const rtFl4WrapperRear = document.querySelector(".fl4-w-2 .rt-fl4-wr");
+const rtFlame4Rear = document.querySelector(".fl4-w-2 .rt-fl4-wr .flame4");
 
 function showDescription() {
   if (headingTimeout) {
@@ -64,43 +72,45 @@ function hideDescription() {
   }, 100);
 }
 
+function startFl4sStaggered() {
+  ltFlame4.style.setProperty(
+    "--fl4-anim-timing",
+    `${getRandomNumber(1.45, 1.85)}s`
+  );
+  ltFlame4.classList.add("flame-4-spark");
+  ltFlame4Rear.style.setProperty(
+    "--fl4-anim-timing",
+    `${getRandomNumber(1.45, 1.85)}s`
+  );
+  ltFlame4Rear.classList.add("flame-4-spark");
+  rtFlame4.style.setProperty(
+    "--fl4-anim-timing",
+    `${getRandomNumber(1.45, 1.85)}s`
+  );
+  rtFlame4.classList.add("flame-4-spark");
+  rtFlame4Rear.style.setProperty(
+    "--fl4-anim-timing",
+    `${getRandomNumber(1.45, 1.85)}s`
+  );
+  rtFlame4Rear.classList.add("flame-4-spark");
+}
+
+function fadeInFl4s() {
+  ltFl4Wrapper.classList.add("front-fade-in");
+  rtFl4Wrapper.classList.add("front-fade-in");
+  ltFl4WrapperRear.classList.add("rear-fade-in");
+  rtFl4WrapperRear.classList.add("rear-fade-in");
+}
+
 cards.forEach((card) => {
   card.addEventListener("mouseover", showDescription);
   card.addEventListener("mouseout", hideDescription);
 });
-
-// see if we can get two flames up and running
-
-const ltFl4Wrapper = document.querySelector(".fl4-w-1 .lt-fl4-wr");
-const ltFlame4 = document.querySelector(".fl4-w-1 .lt-fl4-wr .flame4");
-const rtFl4Wrapper = document.querySelector(".fl4-w-1 .rt-fl4-wr");
-const rtFlame4 = document.querySelector(".fl4-w-1 .rt-fl4-wr .flame4");
-const ltFl4WrapperRear = document.querySelector(".fl4-w-2 .lt-fl4-wr");
-const ltFlame4Rear = document.querySelector(".fl4-w-2 .lt-fl4-wr .flame4");
-const rtFl4WrapperRear = document.querySelector(".fl4-w-2 .rt-fl4-wr");
-const rtFlame4Rear = document.querySelector(".fl4-w-2 .rt-fl4-wr .flame4");
 
 populateSlides(ltFlame4, 52, "flame4", "flames", true);
 populateSlides(rtFlame4, 52, "flame4", "flames", true);
 populateSlides(ltFlame4Rear, 52, "flame4", "flames", true);
 populateSlides(rtFlame4Rear, 52, "flame4", "flames", true);
 
-// stagger them, timeout(s) to start
-// add spark class, raise opacity
-
-setTimeout(() => {
-  ltFlame4.classList.add("flame-4-spark");
-  rtFlame4Rear.classList.add("flame-4-spark");
-}, getRandomNumber(1000, 1500));
-
-setTimeout(() => {
-  rtFlame4.classList.add("flame-4-spark");
-  ltFlame4Rear.classList.add("flame-4-spark");
-}, getRandomNumber(1250, 1750));
-
-setTimeout(() => {
-  ltFl4Wrapper.classList.add("flame-4-fade-in");
-  rtFl4Wrapper.classList.add("flame-4-fade-in");
-  ltFl4WrapperRear.classList.add("flame-4-fade-in");
-  rtFl4WrapperRear.classList.add("flame-4-fade-in");
-}, 1800);
+startFl4sStaggered();
+setTimeout(fadeInFl4s, 500);
