a7387579dab5e3f9f8f597e21043c70b3dd10979
Steve <138328427+smajewski1980@users.noreply.github.com>
2024-11-02 14:24:27 -0400

    adjusted spacing on desc and desc title on proj page

--- projects/projects_script.js
+++ projects/projects_script.js
@@ -13,27 +13,27 @@ const descriptions = [
   },
   {
     title: "My Music Catalog v1",
-    desc: "Version one, before I knew any JS. I exported PDFs from my database and embedded them in iframes.",
+    desc: "Version one was made before I knew any Javascript.</br>I exported PDFs from my database and embedded them in iframes.",
   },
   {
     title: "My Music Catalog v2",
-    desc: "Built with React.js, it is my current catalog and is a work in progress. Yes, I really have all this.",
+    desc: "Built with React.js, it is my current catalog and is a work in progress.</br>This one includes a 10x10 storage unit with all of this music inside.",
   },
   {
     title: "New York State Assembly Home Page",
-    desc: "What a great page for a portfolio!</br>This one includes a carousel of links and an interactive calendar, it is also responsive.",
+    desc: "What a great page for a portfolio!</br>This one includes a carousel of links and an interactive calendar.</br>It is also responsive.",
   },
   {
     title: "Assembly Sound Off(the floor)",
-    desc: "This was a fun music app I made for a few laughs around the office. This one includes you gettin' down!",
+    desc: "This was a fun music app I made for a few laughs in the office.</br>This one includes you gettin' down!",
   },
   {
     title: "Empire State Aerosciences Museum Visit Page",
-    desc: "Went there to visit, thought this would be a fun page to build. This one includes a few modals and an image gallery.",
+    desc: "I went there and thought this would be a fun page to build.</br>This one includes a few modals and an image gallery.",
   },
   {
     title: "Bookmark Landing Page",
-    desc: "A Frontend Mentor project. This one includes a section with tabs that switch content and a faq accordion.",
+    desc: "A Frontend Mentor project.</br>This one includes a section with tabs that switch content and a faq accordion.",
   },
 ];
 
