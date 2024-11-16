e001d6de9522753d4926f8cab96c0758cf2d5e10
Steve <138328427+smajewski1980@users.noreply.github.com>
2024-11-16 07:04:48 -0500

    changed behavior of skip anim button

--- index_script.js
+++ index_script.js
@@ -29,42 +29,38 @@ function flame2Seq() {
       populateSlides(flame2_3, 14, "flame2", "flames");
     }, 350);
   }, 350);
-  timeouts.push(
+
+  setTimeout(() => {
+    runFlame2(flame2_1, textReveal_1, "1");
+    churchBell1.play();
     setTimeout(() => {
-      runFlame2(flame2_1, textReveal_1, "1");
-      churchBell1.play();
+      textReveal_1.classList.add("text-reveal-move-1-final");
+    }, 9000);
+
+    setTimeout(() => {
+      runFlame2(flame2_2, textReveal_2, "2");
+      churchBell2.play();
       setTimeout(() => {
-        textReveal_1.classList.add("text-reveal-move-1-final");
-      }, 9000);
-      timeouts.push(
+        textReveal_2.classList.add("text-reveal-move-2-final");
+      }, 7050);
+      setTimeout(() => {
+        runFlame2(flame2_3, textReveal_3, "3");
         setTimeout(() => {
-          runFlame2(flame2_2, textReveal_2, "2");
-          churchBell2.play();
+          bell3.play();
+        }, 300);
+        setTimeout(() => {
+          setBodyBg();
+          runSmokePuff2();
+        }, 4975);
+        setTimeout(() => {
+          textReveal_3.classList.add("text-reveal-move-3-final");
           setTimeout(() => {
-            textReveal_2.classList.add("text-reveal-move-2-final");
-          }, 7050);
-          timeouts.push(
-            setTimeout(() => {
-              runFlame2(flame2_3, textReveal_3, "3");
-              setTimeout(() => {
-                bell3.play();
-              }, 300);
-              setTimeout(() => {
-                setBodyBg();
-                runSmokePuff2();
-              }, 4975);
-              setTimeout(() => {
-                textReveal_3.classList.add("text-reveal-move-3-final");
-                setTimeout(() => {
-                  finalSeq();
-                }, 400);
-              }, 5050);
-            }, 2000)
-          );
-        }, 1900)
-      );
-    }, 1650)
-  );
+            finalSeq();
+          }, 400);
+        }, 5050);
+      }, 2000);
+    }, 1900);
+  }, 1650);
 }
 
 function runFlame3(flame3) {
@@ -87,21 +83,15 @@ function flame5Seq() {
   populateSlides(flame5_1, 83, "flame5", "flames");
   populateSlides(flame5_2, 83, "flame5", "flames");
   populateSlides(flame5_3, 83, "flame5", "flames");
-  timeouts.push(
+  setTimeout(() => {
+    runFlame5(flame5_1);
     setTimeout(() => {
-      runFlame5(flame5_1);
-      timeouts.push(
-        setTimeout(() => {
-          runFlame5(flame5_2);
-          timeouts.push(
-            setTimeout(() => {
-              runFlame5(flame5_3);
-            }, 2000)
-          );
-        }, 2000)
-      );
-    }, 2500)
-  );
+      runFlame5(flame5_2);
+      setTimeout(() => {
+        runFlame5(flame5_3);
+      }, 2000);
+    }, 2000);
+  }, 2500);
 }
 
 function runFlame6(flame6) {
@@ -123,37 +113,31 @@ function flame8Seq() {
   populateSlides(flame8_1, 31, "flame8", "flames");
   populateSlides(flame8_2, 31, "flame8", "flames");
   populateSlides(flame8_3, 31, "flame8", "flames");
-  timeouts.push(
+  setTimeout(() => {
+    runFlame8(flame8_1);
+    thickSlime.play();
     setTimeout(() => {
-      runFlame8(flame8_1);
+      flame8_1.parentElement.remove();
+    }, 3000);
+    setTimeout(() => {
+      runFlame8(flame8_2);
+      thickSlime.volume = 0.2;
+      thickSlime.load();
       thickSlime.play();
       setTimeout(() => {
-        flame8_1.parentElement.remove();
+        flame8_2.parentElement.remove();
       }, 3000);
-      timeouts.push(
+      setTimeout(() => {
+        runFlame8(flame8_3);
+        thickSlime.volume = 0.3;
+        thickSlime.load();
+        thickSlime.play();
         setTimeout(() => {
-          runFlame8(flame8_2);
-          thickSlime.volume = 0.2;
-          thickSlime.load();
-          thickSlime.play();
-          setTimeout(() => {
-            flame8_2.parentElement.remove();
-          }, 3000);
-          timeouts.push(
-            setTimeout(() => {
-              runFlame8(flame8_3);
-              thickSlime.volume = 0.3;
-              thickSlime.load();
-              thickSlime.play();
-              setTimeout(() => {
-                flame8_3.parentElement.remove();
-              }, 3000);
-            }, 2000)
-          );
-        }, 2000)
-      );
-    }, 3000)
-  );
+          flame8_3.parentElement.remove();
+        }, 3000);
+      }, 2000);
+    }, 2000);
+  }, 3000);
 }
 
 function runSmokePuff() {
@@ -214,38 +198,32 @@ function finalSeq() {
 function MasterSeq() {
   populateSlides(smokePuff, 7, "puff1", "smoke");
   btnEnter.classList.add("btn-fade");
+  btnSkipAnim.classList.add("btn-fade");
   setTimeout(() => {
     btnEnter.remove();
+    btnSkipAnim.remove();
   }, 750);
-  timeouts.push(
-    timeouts.push(
-      setTimeout(() => {
-        sparkClose.play();
-        runSmokePuff();
-      }, 250)
-    )
-  );
+  setTimeout(() => {
+    sparkClose.play();
+    runSmokePuff();
+  }, 250);
   runFlame6(flame6_1);
   runFlame6(flame6_2);
-  timeouts.push(
+  setTimeout(() => {
+    populateSlides(flame6_3, 24, "flame6", "flames");
+    populateSlides(flame6_4, 24, "flame6", "flames");
+    runFlame6(flame6_3);
+    runFlame6(flame6_4);
+    flame6_1W.classList.add("fl-6-1-move-y");
+    flame6_1W_X.classList.add("fl-6-1-move-x");
+    fireworkWhistle.play();
     setTimeout(() => {
-      populateSlides(flame6_3, 24, "flame6", "flames");
-      populateSlides(flame6_4, 24, "flame6", "flames");
-      runFlame6(flame6_3);
-      runFlame6(flame6_4);
-      flame6_1W.classList.add("fl-6-1-move-y");
-      flame6_1W_X.classList.add("fl-6-1-move-x");
-      fireworkWhistle.play();
-      timeouts.push(
-        setTimeout(() => {
-          runFlame3(flame3_6);
-          fireLoop3.play();
-          flame3_6.style.opacity = "1";
-          flame6_1W_X.remove();
-        }, 1000)
-      );
-    }, 3000)
-  );
+      runFlame3(flame3_6);
+      fireLoop3.play();
+      flame3_6.style.opacity = "1";
+      flame6_1W_X.remove();
+    }, 1000);
+  }, 3000);
   setTimeout(() => {
     flame6_2W.classList.add("fl-6-2-move-y");
     flame6_2W_X.classList.add("fl-6-2-move-x");
@@ -263,131 +241,108 @@ function MasterSeq() {
       populateSlides(flame3_4, 49, "flame3", "flames");
     }, 1000);
   }, 4000);
-  timeouts.push(
+  setTimeout(() => {
+    flame6_3W.classList.add("fl-6-3-move-y");
+    flame6_3W_X.classList.add("fl-6-3-move-x");
+    flame6_4W.classList.add("fl-6-4-move-y");
+    flame6_4W_X.classList.add("fl-6-4-move-x");
+    fireworkWhistle.volume = 0.4;
+    fireworkWhistle.load();
+    fireworkWhistle.play();
     setTimeout(() => {
-      flame6_3W.classList.add("fl-6-3-move-y");
-      flame6_3W_X.classList.add("fl-6-3-move-x");
-      flame6_4W.classList.add("fl-6-4-move-y");
-      flame6_4W_X.classList.add("fl-6-4-move-x");
-      fireworkWhistle.volume = 0.4;
-      fireworkWhistle.load();
-      fireworkWhistle.play();
-      timeouts.push(
-        setTimeout(() => {
-          fireLoop2.play();
-          runFlame3(flame3_3);
-          runFlame3(flame3_4);
-          flame3_3.style.opacity = "1";
-          flame3_4.style.opacity = "1";
-          flame6_3W_X.remove();
-          flame6_4W_X.remove();
-          populateSlides(flame3_1, 49, "flame3", "flames");
-          populateSlides(flame3_2, 49, "flame3", "flames");
-        }, 1000)
-      );
-    }, 5000)
-  );
-  timeouts.push(
+      fireLoop2.play();
+      runFlame3(flame3_3);
+      runFlame3(flame3_4);
+      flame3_3.style.opacity = "1";
+      flame3_4.style.opacity = "1";
+      flame6_3W_X.remove();
+      flame6_4W_X.remove();
+      populateSlides(flame3_1, 49, "flame3", "flames");
+      populateSlides(flame3_2, 49, "flame3", "flames");
+    }, 1000);
+  }, 5000);
+  setTimeout(() => {
+    flame6_5W.classList.add("fl-6-5-move-y");
+    flame6_5W_X.classList.add("fl-6-5-move-x");
+    flame6_6W.classList.add("fl-6-5-move-y");
+    flame6_6W_X.classList.add("fl-6-6-move-x");
+    fireworkWhistle.volume = 0.6;
+    fireworkWhistle.play();
     setTimeout(() => {
-      flame6_5W.classList.add("fl-6-5-move-y");
-      flame6_5W_X.classList.add("fl-6-5-move-x");
-      flame6_6W.classList.add("fl-6-5-move-y");
-      flame6_6W_X.classList.add("fl-6-6-move-x");
-      fireworkWhistle.volume = 0.6;
-      fireworkWhistle.play();
-      timeouts.push(
-        setTimeout(() => {
-          fireLoop1.play();
-          fireLoop3.load();
-          runFlame3(flame3_1);
-          runFlame3(flame3_2);
-          flame3_1.style.opacity = "1";
-          flame3_2.style.opacity = "1";
-          flame6_5W_X.remove();
-          flame6_6W_X.remove();
-        }, 1000)
-      );
-      flame5Seq();
-      flame8Seq();
-      populateNavLinksText();
-    }, 7000)
-  );
-  timeouts.push(
+      fireLoop1.play();
+      fireLoop3.load();
+      runFlame3(flame3_1);
+      runFlame3(flame3_2);
+      flame3_1.style.opacity = "1";
+      flame3_2.style.opacity = "1";
+      flame6_5W_X.remove();
+      flame6_6W_X.remove();
+    }, 1000);
+    flame5Seq();
+    flame8Seq();
+    populateNavLinksText();
+  }, 7000);
+  setTimeout(() => {
+    flame2Seq();
+    populateSlides(flame6_1_rnd2, 24, "flame6", "flames");
+    runFlame6(flame6_1_rnd2);
     setTimeout(() => {
-      flame2Seq();
-      populateSlides(flame6_1_rnd2, 24, "flame6", "flames");
-      runFlame6(flame6_1_rnd2);
-      timeouts.push(
+      floorSpark.play();
+      floorFlame.play();
+      flame6_1_rnd2W.classList.add("fl-6-1-rnd2-move");
+      setTimeout(() => {
+        flame5_1.parentElement.remove();
+      }, 3000);
+      setTimeout(() => {
+        flame6_1_rnd2W.remove();
+      }, 1050);
+    }, 350);
+    setTimeout(() => {
+      populateSlides(flame6_2_rnd2, 24, "flame6", "flames");
+      runFlame6(flame6_2_rnd2);
+      setTimeout(() => {
+        flame6_2_rnd2W.classList.add("fl-6-2-rnd2-move");
+        floorSpark.play();
+        floorFlame.play();
         setTimeout(() => {
+          flame5_2.parentElement.remove();
+        }, 2500);
+        setTimeout(() => {
+          flame6_2_rnd2W.remove();
+        }, 550);
+      }, 350);
+      setTimeout(() => {
+        populateSlides(flame6_3_rnd2, 24, "flame6", "flames");
+        runFlame6(flame6_3_rnd2);
+        setTimeout(() => {
+          flame6_3_rnd2W.classList.add("fl-6-3-rnd2-move");
           floorSpark.play();
           floorFlame.play();
-          flame6_1_rnd2W.classList.add("fl-6-1-rnd2-move");
           setTimeout(() => {
-            flame5_1.parentElement.remove();
+            flame5_3.parentElement.remove();
           }, 3000);
           setTimeout(() => {
-            flame6_1_rnd2W.remove();
+            flame6_3_rnd2W.remove();
           }, 1050);
-        }, 350)
-      );
-      timeouts.push(
-        setTimeout(() => {
-          populateSlides(flame6_2_rnd2, 24, "flame6", "flames");
-          runFlame6(flame6_2_rnd2);
-          timeouts.push(
-            setTimeout(() => {
-              flame6_2_rnd2W.classList.add("fl-6-2-rnd2-move");
-              floorSpark.play();
-              floorFlame.play();
-              setTimeout(() => {
-                flame5_2.parentElement.remove();
-              }, 2500);
-              setTimeout(() => {
-                flame6_2_rnd2W.remove();
-              }, 550);
-            }, 350)
-          );
-          timeouts.push(
-            setTimeout(() => {
-              populateSlides(flame6_3_rnd2, 24, "flame6", "flames");
-              runFlame6(flame6_3_rnd2);
-              timeouts.push(
-                setTimeout(() => {
-                  flame6_3_rnd2W.classList.add("fl-6-3-rnd2-move");
-                  floorSpark.play();
-                  floorFlame.play();
-                  setTimeout(() => {
-                    flame5_3.parentElement.remove();
-                  }, 3000);
-                  setTimeout(() => {
-                    flame6_3_rnd2W.remove();
-                  }, 1050);
-                }, 350)
-              );
-            }, 2000)
-          );
-        }, 2000)
-      );
+        }, 350);
+      }, 2000);
+    }, 2000);
+    setTimeout(() => {
+      btnSkipAnim.classList.add("btn-fade");
       setTimeout(() => {
-        btnSkipAnim.classList.add("btn-fade");
+        btnSkipAnim.remove();
         setTimeout(() => {
-          btnSkipAnim.remove();
-          setTimeout(() => {
-            animWrapper.remove();
-          }, 1000);
-        }, 750);
-      }, 11000);
-    }, 9000)
-  );
+          animWrapper.remove();
+        }, 1000);
+      }, 750);
+    }, 11000);
+  }, 9000);
 }
 
 function abortAnimation() {
   animWrapper.remove();
   btnSkipAnim.remove();
   btnEnter.remove();
-  for (let i = 0; i < timeouts.length; i++) {
-    clearTimeout(timeouts[i]);
-  }
   setBodyBg();
   finalSeq();
 }
